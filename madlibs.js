ff9966e8a8a3021c29edfffb9780a277cda58f6b
dinahimed1 <dinah4452@tywls.org>
2016-01-07 21:45:03 +0000

    the second version of my madlibs game.

--- madlibs.js
+++ madlibs.js
@@ -1,15 +1,35 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = " You are a noun. YOu enjoy verb and verb with adjective gorillas. You have so much fun that you forgot about the adjective gorillas. You feel adjective so you go home. But then the adjective gorillas go and find you to seek revenge. Once the gorilas find you, you verb. Gucci Flip Flops pops up and verb. ";
+  var story = " You are a noun. You enjoy verb1 and verb2 with adjective1 gorillas. You have so much fun that you forgot about the adjective2 gorillas. You feel adjective3 so you go home. But then the adjective4 gorillas go and find you to seek revenge. Once the gorilas find you, you verb3.  Gucci Flip Flops pops up and verb4.";
   
  // You are a (noun). YOu enjoy (verb1) and (verb2) with (adjective1) gorillas. You have so much fun that you forgot about the (adjective2) gorillas. You feel (adjective3) so you go home. But then the (adjective4) gorillas go and find you to seek revenge. Once the gorilas find you, you (verb3). Gucci Flip Flops pops up and (verb4). 
 
   var inputValue = document.getElementById("noun").value;
   story = story.replace("noun", inputValue);
   
-  inputValue = document.getElementById("verb").value;
-  story = story.replace("VERB", inputValue);
+  inputValue = document.getElementById("verb1").value;
+  story = story.replace("verb1", inputValue);
+  
+    inputValue = document.getElementById("verb2").value;
+  story = story.replace("verb1", inputValue);
+  
+    inputValue = document.getElementById("adjective1").value;
+  story = story.replace("verb1", inputValue);
+  
+    inputValue = document.getElementById("adjective2").value;
+  story = story.replace("verb1", inputValue);
+  
+    inputValue = document.getElementById("adjective3").value;
+  story = story.replace("verb1", inputValue);
+  
+    inputValue = document.getElementById("verb3").value;
+  story = story.replace("verb1", inputValue);
+  
+    inputValue = document.getElementById("verb4").value;
+  story = story.replace("verb1", inputValue);
+  
+  
   
   // This writes the story when the user clicks the "Generate MadLib" button"
   document.getElementById("result").innerHTML = story;
