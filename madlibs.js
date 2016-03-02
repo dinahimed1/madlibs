63e719e43a9b4dc9c9e1e135656d9fa8d69bce7f
dinahimed1 <dinah4452@tywls.org>
2016-03-02 21:48:51 +0000

    revised version

--- madlibs.js
+++ madlibs.js
@@ -1,9 +1,9 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = " You are a noun. You enjoy verb1 and verb2 with adjective1 gorillas. You have so much fun that you forgot about the adjective2 gorillas. You feel adjective3 so you go home. But then the adjective4 gorillas go and find you to seek revenge. Once the gorilas find you, you verb3.  Gucci Flip Flops pops up and verb4.";
+  var story = " You are a noun. You enjoy verb1 and verb2 with adjective1 friends. You have so much fun that you forgot about the adjective2 homework.You feel adjective3 so you leave your home. But then your adjective4 parents go and find you. Once your parents find you, you look at the verb3. You look at the verb4 to find a way to escape.";
   
- // You are a (noun). YOu enjoy (verb1) and (verb2) with (adjective1) gorillas. You have so much fun that you forgot about the (adjective2) gorillas. You feel (adjective3) so you go home. But then the (adjective4) gorillas go and find you to seek revenge. Once the gorilas find you, you (verb3). Gucci Flip Flops pops up and (verb4). 
+ // You are a (noun). YOu enjoy (verb1) and (verb2) with (adjective1) friends. You have so much fun that you forgot about the (adjective2) homework. You feel (adjective3) so you leave home. But then your (adjective4) parents go and find you. Once your parents find you, you look at the (verb3). You look at the (verb4) to find a way to escape. 
 
   var inputValue = document.getElementById("noun").value;
   story = story.replace("noun", inputValue);
