dfd0ccc64ead5ec9e0e20fb3db45c34f489877b3
dinahimed1 <dinah4452@tywls.org>
2016-03-03 21:30:46 +0000

    revised again

--- madlibs.js
+++ madlibs.js
@@ -1,9 +1,9 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = " You are a noun. You enjoy verb1 and verb2 with adjective1 friends. You have so much fun that you forgot about the adjective2 homework.You feel adjective3 so you leave your home. But then your adjective4 parents go and find you. Once your parents find you, you look at the verb3. You look at the verb4 to find a way to escape.";
+  var story = " You are a noun. You enjoy verb1 and verb2 with adjective1 friends. You have so much fun that you forgot about the adjective2 homework.You feel adjective3 so you leave your home. But then your parents go and find you. Once your parents find you, you look at the verb3. You look at the verb4 to find a way to escape.";
   
- // You are a (noun). YOu enjoy (verb1) and (verb2) with (adjective1) friends. You have so much fun that you forgot about the (adjective2) homework. You feel (adjective3) so you leave home. But then your (adjective4) parents go and find you. Once your parents find you, you look at the (verb3). You look at the (verb4) to find a way to escape. 
+ // You are a (noun). YOu enjoy (verb1) and (verb2) with (adjective1) friends. You have so much fun that you forgot about the (adjective2) homework. You feel (adjective3) so you leave home. But then your  parents go and find you. Once your parents find you, you look at the (verb3). You look at the (verb4) to find a way to escape. 
 
   var inputValue = document.getElementById("noun").value;
   story = story.replace("noun", inputValue);
@@ -12,22 +12,22 @@ function generateMadlib() {
   story = story.replace("verb1", inputValue);
   
     inputValue = document.getElementById("verb2").value;
-  story = story.replace("verb1", inputValue);
+  story = story.replace("verb2", inputValue);
   
     inputValue = document.getElementById("adjective1").value;
-  story = story.replace("verb1", inputValue);
+  story = story.replace("adjective1", inputValue);
   
     inputValue = document.getElementById("adjective2").value;
-  story = story.replace("verb1", inputValue);
+  story = story.replace("adjective2", inputValue);
   
     inputValue = document.getElementById("adjective3").value;
-  story = story.replace("verb1", inputValue);
+  story = story.replace("adjective3", inputValue);
   
     inputValue = document.getElementById("verb3").value;
-  story = story.replace("verb1", inputValue);
+  story = story.replace("verb3", inputValue);
   
     inputValue = document.getElementById("verb4").value;
-  story = story.replace("verb1", inputValue);
+  story = story.replace("verb4", inputValue);
   
   
   
